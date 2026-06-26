c8c63b29037239412ee274bd86a19618b7350350
caleblee475-lab <caleblee475@gmail.com>
2026-06-27 02:33:04 +0800

    fix 1?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://caleblee475-lab.github.io',
-  base: '/economics-site/',
+  base: '/economics-site/', // Notice the leading AND trailing slash
 });
