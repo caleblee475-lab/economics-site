b2256075eac4828ee24d43feafda131d74cfe554
caleblee475-lab <caleblee475@gmail.com>
2026-06-27 02:29:38 +0800

    this should fix it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://caleblee475-lab.github.io',
-  base: 'economics-site',
+  base: '/economics-site/',
 });
